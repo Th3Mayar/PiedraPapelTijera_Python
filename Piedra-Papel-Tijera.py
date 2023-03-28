647a300c2fd73264e6c443e54661dece882c00e3
José F. Henríquez <108672422+Th3Mayar@users.noreply.github.com>
2023-03-28 13:08:46 -0300

    Update...

---
+++ Piedra-Papel-Tijera.py
@@ -0,0 +1,249 @@
+#-------------------------------------#
+#               Reglas                #
+#-------------------------------------#
+#     Tijeras corta papel             #
+#     Papel cubre piedra              #
+#     Piedra aplasta a lagarto        #
+#     Lagarto envenena a Spock        #
+#     Spock rompe las tijeras         #
+#     Tijeras decapitan al lagarto    #
+#     Lagarto se come el papel        #
+#     Papel desaprueba a Spock        #
+#     Spock vaporiza a piedra         #
+#     Piedra aplasta a tijera         #
+#-------------------------------------#
+
+#-------------------------------------#
+#               Rules                 #
+#-------------------------------------#
+# Scissors cut paper                  #
+# Paper covers stone                  #
+# Stone crushes lizard                #
+# Lizard poisons Spock                #
+# Spock breaks the scissors           #
+# Scissors decapitate the lizard      #
+# Lizard eats paper                   #
+# Paper disapproves of Spock          #
+# Spock vaporizes stone               #
+# Rock crushes scissors               #
+#-------------------------------------#
+
+# Import libraries
+from ast import main
+from random import *
+import os
+
+# Funcion principal, main
+def fStart():
+    
+    vIntentos = 0 # Intentos maximos van a ser 3, por ende lo inicializamos en 0 mientras.
+    vVida = 0 # La vida maxima es para el jugador, porque este juego es el user vs la maquina
+    while True:
+        # Menu
+        os.system("color 0B")
+        os.system("cls")
+        print("""
+                ┌─────────────────────────────────────────────────────────┐
+                │     Bienvenidos al juego de piedra papel y tijera...    │
+                │_________________________________________________________│
+                │                                                         │
+                │    1- Jugar                                             │
+                │    2- Reglas                                            │
+                │    3- Salir                                             │
+                │                                                         │
+                └─────────────────────────────────────────────────────────┘ 
+        """)
+        vOp = int(input("¿Cual eliges? ")) # Se puede elegir entre 3 opciones dependiendo que se elija, las condiciones seran las siguientes.
+
+        if (vOp == 1): # Si es = a 1.
+            while True:
+                os.system("color 0A")
+                os.system("cls")
+                vUsuario1 = []
+                #vUsuario2 = [] # Por si queremos un 1 vs 1, de momento no se ha implementado
+                vNombres = ['Piedra', 'Papel', 'Tijera', 'Lagarto', 'Spock']
+                Maquina = ['Piedra', 'Papel', 'Tijera', 'Lagarto', 'Spock']
+
+                # Caracteres aleatorios de la maquina
+                Aleatorio = randrange(len(Maquina))
+                Maquina = Maquina[Aleatorio]
+
+                print("""
+                ┌─────────────────────────────────────────────────────────┐
+                │     Bienvenidos al juego de piedra papel y tijera...    │
+                │_________________________________________________________│
+                │                                                         │
+                │    1- Piedra                                            │
+                │    2- Papel                                             │
+                │    3- Tijera                                            │
+                │    4- Lagarto                                           │
+                │    5- Spock                                             │
+                │    6- Salir                                             │
+                │                                                         │
+                └─────────────────────────────────────────────────────────┘ 
+                    """)
+                vOp = int(input("¿Cual eliges? "))
+
+                if (vOp == 1):
+                    vUsuario1 = vNombres[0]
+                    
+                elif (vOp == 2):
+                    vUsuario1 = vNombres[1]
+                    
+                elif (vOp == 3):
+                    vUsuario1 = vNombres[2]
+                    
+                elif (vOp == 4):
+                    vUsuario1 = vNombres[3]
+                    
+                elif (vOp == 5):
+                    vUsuario1 = vNombres[4]
+                    
+                elif (vOp == 6):
+                    print("\nGracias por jugar")
+                    os.system("pause")
+                    fStart()
+
+                else:
+                    print("Opcion no valida, intentelo de nuevo")
+
+                #Jugador principal
+                print("")
+
+                if (vUsuario1 == vNombres[1] and Maquina == vNombres[0]):
+                    print("Ganaste, papel tapa piedra")
+                    vIntentos = vIntentos + 1
+
+                elif (vUsuario1 == vNombres[2] and Maquina == vNombres[1]):
+                    print("Ganaste, Tijera corta papel")
+                    vIntentos = vIntentos + 1
+
+                elif (vUsuario1 == vNombres[0] and Maquina == vNombres[2]):
+                    print("Ganaste, Piedra destruye tijera")
+                    vIntentos = vIntentos + 1
+
+                elif (vUsuario1 == vNombres[0] and Maquina == vNombres[3]):
+                    print("Ganaste, piedra aplasta a lagarto")
+                    vIntentos = vIntentos + 1
+
+                elif (vUsuario1 == vNombres[3] and Maquina == vNombres[4]):
+                    print("Ganaste, lagarto envenena a spock")
+                    vIntentos = vIntentos + 1
+
+                elif (vUsuario1 == vNombres[3] and Maquina == vNombres[1]):
+                    print("Ganaste, lagarto devora a papel")
+                    vIntentos = vIntentos + 1
+
+                elif (vUsuario1 == vNombres[2] and Maquina == vNombres[3]):
+                    print("Ganaste, tijera decapita a lagarto")
+                    vIntentos = vIntentos + 1
+
+                elif (vUsuario1 == vNombres[4] and Maquina == vNombres[2]):
+                    print("Ganaste, Spock rompe a tijera")
+                    vIntentos = vIntentos + 1
+
+                elif (vUsuario1 == vNombres[1] and Maquina == vNombres[4]):
+                    print("Ganaste, papel desautoriza a Spock")
+                    vIntentos = vIntentos + 1
+
+                elif (vUsuario1 == vNombres[4] and Maquina == vNombres[0]):
+                    print("Ganaste, Spock vaporiza a piedra")
+                    vIntentos = vIntentos + 1
+
+                #Jugador maquina 
+                if (Maquina == vNombres[1] and vUsuario1 == vNombres[0]):
+                    print("Perdiste, papel tapa piedra")
+                    vVida = vVida + 1
+
+                elif (Maquina == vNombres[2] and vUsuario1 == vNombres[1]):
+                    print("Perdiste, Tijera corta papel")
+                    vVida = vVida + 1
+
+                elif (Maquina == vNombres[0] and vUsuario1 == vNombres[2]):
+                    print("Perdiste, Piedra destruye tijera")
+                    vVida = vVida + 1
+
+                elif (Maquina == vNombres[0] and vUsuario1 == vNombres[3]):
+                    print("Perdiste, piedra aplasta a lagarto")
+                    vVida = vVida + 1
+
+                elif (Maquina == vNombres[3] and vUsuario1 == vNombres[4]):
+                    print("Perdiste, lagarto envenena a spock")
+                    vVida = vVida + 1
+
+                elif (Maquina == vNombres[3] and vUsuario1 == vNombres[1]):
+                    print("Perdiste, lagarto devora a papel")
+                    vVida = vVida + 1
+
+                elif (Maquina == vNombres[2]and vUsuario1 == vNombres[3]):
+                    print("Perdiste, tijera decapita a lagarto")
+                    vVida = vVida + 1
+
+                elif (Maquina == vNombres[4] and vUsuario1 == vNombres[2]):
+                    print("Perdiste, Spock rompe a tijera")
+                    vVida = vVida + 1
+
+                elif (Maquina == vNombres[1] and vUsuario1 == vNombres[4]):
+                    print("Perdiste, papel desautoriza a Spock")
+                    vVida = vVida + 1
+
+                elif (Maquina == vNombres[4] and vUsuario1 == vNombres[0]):
+                    print("Perdiste, Spock vaporiza a piedra")
+                    vVida = vVida + 1
+
+                #Empate
+                elif (Maquina == vUsuario1):
+                    print("Empate")
+
+                #Intentos
+                if (vOp != 6 and vOp != 7 and vOp <= 7):
+                    print(f"\nGanadas por el usuario: {vIntentos}/3")
+                    print(f"Ganadas por la maquina: {vVida}/3\n")
+
+                #Finalizar ciclo * las vidas
+                if (vVida == 3 or vIntentos == 3):
+
+                    while True:
+
+                        Salir = input("S -> Salir, N -> Seguir intentando. S/N: ").upper()
+
+                        if (Salir == 'N'):
+                            vIntentos = 0
+                            vVida = 0
+                            break
+
+                        elif (Salir == 'S'):
+                            fStart()
+
+                        else:
+                            print("Opcion no valida")
+                            
+
+                os.system("pause")
+
+        elif (vOp == 2):
+            print("""
+                Reglas del game:
+                ─────────────────────────────────────────────────────────── 
+                - Tijeras corta papel               
+                - Papel cubre piedra
+                - Piedra aplasta a lagarto
+                - Lagarto envenena a Spock
+                - Spock rompe las tijeras 
+                - Tijeras decapitan al lagarto
+                - Lagarto se come el papel
+                - Papel desaprueba a Spock
+                - Spock vaporiza a piedra
+                - Piedra aplasta a tijera
+                ─────────────────────────────────────────────────────────── 
+                    """)
+            os.system("pause")
+        
+        elif (vOp == 3):
+            exit()
+        
+        else:
+            print("Opcion no valida")
+            os.system('pause')
+
+fStart()
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # PiedraPapelTijera_Python
 Este juego tiene como objetivo el mismo juego de piedra papel tijera lagarto y spock pero en este caso, este esta hecho en python con condicionales y estas mismas están controladas, además de eso, las reglas no cambian.
+
+---
+
+#### It works in the following way... (Look at the documented file...)
+
+---
+
+### Create by:
++ ##### Jose Francisco H.
+
+
